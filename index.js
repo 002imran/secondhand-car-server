2248cde3d2c43239c68a7a3346595c01b660a7b5
alimranhossain <alimranhossain44@gmail.com>
2022-11-24 19:18:11 +0600

    mongo db connected

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 
@@ -9,6 +10,12 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6xivgke.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 app.get('/', async (req, res) => {
     res.send('zCar server is running')
