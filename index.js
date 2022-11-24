241edf0b15ea26f38a915f2f1b0e3e66afe471a3
alimranhossain <alimranhossain44@gmail.com>
2022-11-24 23:07:43 +0600

    cagegories data call by categories api

--- index.js
+++ index.js
@@ -15,6 +15,26 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+        const categoriesCollection = client.db('zCar').collection('categorie');
+        
+        //api to get categories data
+        app.get('/categories', async(req, res) => {
+            const query = {};
+            const categories = await categoriesCollection.find(query).toArray();
+            res.send(categories);
+        
+        })
+
+    }
+    finally{
+
+    }
+}
+
+run().catch((err) => console.log(err));
+
 
 
 app.get('/', async (req, res) => {
